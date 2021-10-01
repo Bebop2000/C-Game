920524c6969fdbf1f85599f4028eb20a708378c0
Bebop2000 <schnumma@gmail.com>
2021-10-01 16:42:13 +0100

    suck

---
+++ build/CMakeFiles/CGame.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CGame"
+  "CGame.pdb"
+  "CMakeFiles/CGame.dir/src/engine/src/blocks/block.c.o"
+  "CMakeFiles/CGame.dir/src/engine/src/blocks/blockTextures.c.o"
+  "CMakeFiles/CGame.dir/src/engine/src/camera.c.o"
+  "CMakeFiles/CGame.dir/src/engine/src/input.c.o"
+  "CMakeFiles/CGame.dir/src/engine/src/render.c.o"
+  "CMakeFiles/CGame.dir/src/engine/src/shaderProgram.c.o"
+  "CMakeFiles/CGame.dir/src/engine/src/stb_image.c.o"
+  "CMakeFiles/CGame.dir/src/engine/src/texture.c.o"
+  "CMakeFiles/CGame.dir/src/engine/src/window.c.o"
+  "CMakeFiles/CGame.dir/src/engine/vendor/glad.c.o"
+  "CMakeFiles/CGame.dir/src/game/main.c.o"
+  "CMakeFiles/CGame.dir/src/game/scenes/testScene.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
